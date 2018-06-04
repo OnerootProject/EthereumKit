b7e88f0be08d1ab318f6d4c6702a0f59ad99ab0c
siyue <pengpeng.liu@oneroot.io>
2018-06-04 21:25:29 +0800

    Add Unit Testing

---
+++ EthereumKitTests/EthereumKitTests.swift
@@ -0,0 +1,35 @@
+//
+//  EthereumKitTests.swift
+//  EthereumKitTests
+//
+//  Created by 刘朋朋 on 2018/6/4.
+//  Copyright © 2018年 ONEROOT PROJECT. All rights reserved.
+//
+
+import XCTest
+
+class EthereumKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
